3d6ff388f8058d66b343aef9248de7ed1e2d5a3b
Suzon Das <suzonduislm52@gmail.com>
2019-09-26 00:49:33 +0600

    Commented test values, removed unncessessary libraries

--- realtime/main.py
+++ realtime/main.py
@@ -3,57 +3,42 @@ Realtime Monitoring of Zkteco Fingerprint Machines. It receive event from machin
 from database and then show the amalgamated information in GUI Screen. N.B. This is not under any GPL.
 ALl rights reserved by Suzon Das [https://www.github.com/suzon-das]
 '''
-from kivy import Config
 
-Config.set('graphics', 'multisamples', '0')
 # Python Libraries
 import webbrowser
-from os.path import dirname, join
 from threading import Thread
-from time import time, sleep
-from urllib.request import urlopen
 import winsound
 import pyttsx3 as pyttsx3
 import json
-from kivy.core.window import Window
-
-from kivy.uix.scrollview import ScrollView
-
-from utils import *
+from functools import partial
 
 # Kivy libraries [https://kivy.org/]
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.properties import NumericProperty, StringProperty, BooleanProperty, \
-    ListProperty
-from kivy.clock import Clock
-from kivy.animation import Animation
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-from kivy.network.urlrequest import UrlRequest
-from functools import partial
+from urllib.request import Request, urlopen
 
 # ZKTeco Python Libraries [https://github.com/fananimi/pyzk]. Kudos to Fanani M. Ihsan
 import pyzk.pyzk as pyzk
 from pyzk.zkmodules.defs import *
 from pyzk.misc import *
-from kivy.core.audio import SoundLoader
 
+# Set Text-Speech configuration
 engine = pyttsx3.init()
-
-rate = engine.getProperty('rate')
-en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
+rate = engine.getProperty('rate') # speed of speech
 
 # Use female English voice
+en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 engine.setProperty('voice', en_voice_id)
 engine.setProperty('rate', 180)
-engine.say('Welcome to Fitness plus BD')
+engine.say('Welcome to Fitness plus BD')  # Welcome message from application
 engine.runAndWait()
+
 # Zkteco libraries initializing
-ip_address = '103.91.229.62'  # set the ip address of the device to test
+ip_address = '103.91.229.62'  # set the ip address of the Zkteco device to test
 machine_port = 4370
 
 z = pyzk.ZKSS()
@@ -70,9 +55,7 @@ z.enable_realtime()
 
 # Initializing global variables
 evntMsg = ''  # event wise message
-arr = [100388,555, 100291, 100482 ]
-arrs = ['04:22','04:32', '04:45', '04:53' ]
-flag =0
+
 
 # functions to produce record with null value
 def noResponseRecord():
@@ -92,9 +75,9 @@ def noResponseRecord():
 # member id returned from machine and then adding those data to layout row
 def eventThread():
     # wait for event
-    # z.recv_event()
-    # ev = z.get_last_event()
-    ev = EF_ATTLOG
+    z.recv_event()
+    ev = z.get_last_event()
+    # ev = EF_ATTLOG
 
     frequency = 2500  # Set Frequency To 2500 Hertz
     duration = 1000  # Set Duration To 1000 ms == 1 second
@@ -109,26 +92,23 @@ def eventThread():
     # Our main area. It is true when member successfully verified by ZKTeco
     elif ev == EF_ATTLOG:
         evntMsg = 'User Accessed'
-        # t = tuple(z.parse_event_attlog())
-        # userId = t[0]  # Member Id set on machine
-        global flag
-        userId = arr[flag]  # Member Id set on machine
-        # userTime = t[2]  # Time of access
-        userTime = arrs[flag]  # Time of access
+        t = tuple(z.parse_event_attlog())
+        userId = t[0]  # Member Id set on machine
+        userTime = t[2]  # Time of access
 
+        # Exclude any user. Our case we excluded admin
         if userId == 555:
             engine.say('Admin Finger')
             engine.runAndWait()
 
-        from urllib.request import Request, urlopen
-
+        # API response using user id for fetching more data
         req = Request(
             'http://door.fitnessplusbd.com/uttara/users/userSubscription?id=' + str(userId),
             headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
-
         data = json.loads(webpage)
 
+        # We set stat attribute for identifying found and not found user in response
         if data["stat"] == 200:
             engine.say('Hi ' + str(data["name"]) + ', Thank you')
             engine.runAndWait()
@@ -140,28 +120,31 @@ def eventThread():
             layout.add_widget(Label(text=str(data['branch']), size_hint_x=None, width=50))
             layout.add_widget(Label(text=str(userTime), size_hint_x=None, width=150))
 
+            # comparing membership payment (Not Paid, Paid, Partially Paid)
             if str(data['status']) == 'Not Paid':
                 engine.say('Not Paid')
                 engine.runAndWait()
                 layout.add_widget(
                     Label(text=str(data['status']), size_hint_x=None, width=100, color=(1.0, 0.0, 1.0, 1.0),
-                          bold=True, ))
+                          bold=True, ))  # changing color of text
             else:
                 layout.add_widget(Label(text=str(data['status']), size_hint_x=None, width=100))
 
+            # Details button
             layout.add_widget(Button(text='Details', size_hint_x=None, width=80, on_press=partial(webbrowser.open,
                                                                                                   'http://door.fitnessplusbd.com/uttara/users/redirectFromKivy?id=' + str(
                                                                                                       data["id"]))))
 
+            # Comparing Membership expiry date
             d1 = datetime.datetime.strptime(str(data['membership_valid_to']), "%d/%m/%Y").strftime("%Y-%m-%d")
             d2 = str(datetime.datetime.now())
-
             if d2 > d1:
                 engine.say('Membership Expired')
                 engine.runAndWait()
         else:
-            engine.say('Admin Finger')
+            engine.say('The User was not found in Database')
             engine.runAndWait()
+
     elif ev == EF_FINGER:
         evntMsg = 'Someone placed Finger'
     elif ev == EF_ENROLLUSER:
@@ -185,8 +168,6 @@ def eventThread():
         noResponseRecord()
 
     # Callback to own so that the listening event continues after one event
-    flag = flag +1
-    sleep(15)
     eventThread()
 
 
@@ -229,6 +210,7 @@ root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height), bar_w
 root.add_widget(layout)
 
 
+# The app part
 class RealTime(App):
     def build(self):
         listenEvent()
